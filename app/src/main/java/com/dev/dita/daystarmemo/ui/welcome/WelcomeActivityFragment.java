6e41b4cc46085b0674924c4f2fdc9754f13a1da0
Nathan Mkaya <nathanmkaya@gmail.com>
2016-05-14 01:18:45 +0300

    Welcome Activity Initial

---
+++ app/src/main/java/com/dev/dita/daystarmemo/ui/welcome/WelcomeActivityFragment.java
@@ -0,0 +1,24 @@
+package com.dev.dita.daystarmemo.ui.welcome;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.dev.dita.daystarmemo.R;
+
+/**
+ * A placeholder fragment containing a simple view.
+ */
+public class WelcomeActivityFragment extends Fragment {
+
+    public WelcomeActivityFragment() {
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_welcome, container, false);
+    }
+}
